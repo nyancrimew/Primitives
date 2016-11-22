194e69bc3afbb67123726b02b99868c0865cc502
deletescape <deletescape@outlook.com>
2016-11-22 08:48:51 +0100

    add long array classes

---
+++ src/main/java/ch/deletescape/primitives/arrays/PrLongArray.java
@@ -0,0 +1,119 @@
+package ch.deletescape.primitives.arrays;
+
+import ch.deletescape.primitives.PrLong;
+
+public final class PrLongArray {
+  /**
+   * Private Constructor to prevent initialization
+   */
+  private PrLongArray() {
+  }
+
+  /**
+   * Converts a int array to a long array
+   * 
+   * @param iArr
+   *          the int array to convert
+   * @return the resulting long array.
+   */
+  public static long[] from(int[] iArr) {
+    long[] tmp = new long[iArr.length];
+    for (int i = 0; i < iArr.length; i++) {
+      tmp[i] = iArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a char array to a long array
+   * 
+   * @param cArr
+   *          the char array to convert
+   * @return the resulting long array.
+   */
+  public static long[] from(char[] cArr) {
+    long[] tmp = new long[cArr.length];
+    for (int i = 0; i < cArr.length; i++) {
+      tmp[i] = cArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a byte array to a long array
+   * 
+   * @param bArr
+   *          the byte array to convert
+   * @return the resulting long array.
+   */
+  public static long[] from(byte[] bArr) {
+    long[] tmp = new long[bArr.length];
+    for (int i = 0; i < bArr.length; i++) {
+      tmp[i] = bArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a short array to a long array using {@link PrLong#from(long)}
+   * 
+   * @param shrtArr
+   *          the short array to convert
+   * @return the resulting long array.
+   */
+  public static long[] from(short[] iArr) {
+    long[] tmp = new long[iArr.length];
+    for (int i = 0; i < iArr.length; i++) {
+      tmp[i] = iArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a double array to a long array using {@link PrLong#from(double)}
+   * 
+   * @param dblArr
+   *          the double array to convert
+   * @return the resulting long array.
+   * @see PrLong#from(double)
+   */
+  public static long[] from(double[] dblArr) {
+    long[] tmp = new long[dblArr.length];
+    for (int i = 0; i < dblArr.length; i++) {
+      tmp[i] = PrLong.from(dblArr[i]);
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a float array to a long array using {@link PrLong#from(float)}
+   * 
+   * @param fltArr
+   *          the float array to convert
+   * @return the resulting long array.
+   * @see PrLong#from(float)
+   */
+  public static long[] from(float[] fltArr) {
+    long[] tmp = new long[fltArr.length];
+    for (int i = 0; i < fltArr.length; i++) {
+      tmp[i] = PrLong.from(fltArr[i]);
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a boolean array to a long array using {@link PrLong#from(boolean)}
+   * 
+   * @param boolArr
+   *          the boolean array to convert
+   * @return the resulting long array.
+   * @see PrLong#from(boolean)
+   */
+  public static long[] from(boolean[] boolArr) {
+    long[] tmp = new long[boolArr.length];
+    for (int i = 0; i < boolArr.length; i++) {
+      tmp[i] = PrLong.from(boolArr[i]);
+    }
+    return tmp;
+  }
+}
---
+++ src/test/java/ch/deletescape/primitives/arrays/PrLongArrayTest.java
@@ -0,0 +1,47 @@
+package ch.deletescape.primitives.arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PrLongArrayTest {
+  @Test
+  public void fromInt() {
+    assertThat(PrLongArray.from(new int[] { 1, Integer.MAX_VALUE, Integer.MAX_VALUE - 1 }),
+        is(new long[] { 1, 2147483647, 2147483646 }));
+  }
+
+  @Test
+  public void fromShort() {
+    assertThat(PrLongArray.from(new short[] { 1, Short.MAX_VALUE }), is(new long[] { 1, 32767 }));
+  }
+
+  @Test
+  public void fromByte() {
+    assertThat(PrLongArray.from(new byte[] { 1, Byte.MAX_VALUE }), is(new long[] { 1, 127 }));
+  }
+
+  @Test
+  public void fromChar() {
+    assertThat(PrLongArray.from(new char[] { '\0', Character.MAX_VALUE, 1 }), is(new long[] { 0, 65535, 1 }));
+  }
+
+  @Test
+  public void fromDouble() {
+    assertThat(PrLongArray.from(new double[] { 1.0, 1.3, Double.MAX_VALUE }),
+        is(new long[] { 1, 1, 9223372036854775807L }));
+  }
+
+  @Test
+  public void fromFloat() {
+    assertThat(PrLongArray.from(new float[] { 1f, 1.3f, Float.MAX_VALUE }),
+        is(new long[] { 1, 1, 9223372036854775807L }));
+  }
+
+  @Test
+  public void fromBoolean() {
+    assertThat(PrLongArray.from(new boolean[] { true, false }), is(new long[] { 1, 0 }));
+  }
+
+}
