16cf8343d6d88f1b73d1895664b9a66291e90c30
deletescape <deletescape@outlook.com>
2016-11-22 08:41:42 +0100

    add int array classes

---
+++ src/main/java/ch/deletescape/primitives/arrays/PrIntArray.java
@@ -0,0 +1,120 @@
+package ch.deletescape.primitives.arrays;
+
+import ch.deletescape.primitives.PrInt;
+
+public final class PrIntArray {
+  /**
+   * Private Constructor to prevent initialization
+   */
+  private PrIntArray() {
+  }
+
+  /**
+   * Converts a long array to a int array using {@link PrInt#from(long)}
+   * 
+   * @param lngArr
+   *          the long array to convert
+   * @return the resulting int array.
+   * @see PrInt#from(long)
+   */
+  public static int[] from(long[] lngArr) {
+    int[] tmp = new int[lngArr.length];
+    for (int i = 0; i < lngArr.length; i++) {
+      tmp[i] = PrInt.from(lngArr[i]);
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a char array to a int array
+   * 
+   * @param cArr
+   *          the char array to convert
+   * @return the resulting int array.
+   */
+  public static int[] from(char[] cArr) {
+    int[] tmp = new int[cArr.length];
+    for (int i = 0; i < cArr.length; i++) {
+      tmp[i] = cArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a byte array to a int array
+   * 
+   * @param bArr
+   *          the byte array to convert
+   * @return the resulting int array.
+   */
+  public static int[] from(byte[] bArr) {
+    int[] tmp = new int[bArr.length];
+    for (int i = 0; i < bArr.length; i++) {
+      tmp[i] = bArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a short array to a int array using {@link PrInt#from(int)}
+   * 
+   * @param shrtArr
+   *          the short array to convert
+   * @return the resulting int array.
+   */
+  public static int[] from(short[] iArr) {
+    int[] tmp = new int[iArr.length];
+    for (int i = 0; i < iArr.length; i++) {
+      tmp[i] = iArr[i];
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a double array to a int array using {@link PrInt#from(double)}
+   * 
+   * @param dblArr
+   *          the double array to convert
+   * @return the resulting int array.
+   * @see PrInt#from(double)
+   */
+  public static int[] from(double[] dblArr) {
+    int[] tmp = new int[dblArr.length];
+    for (int i = 0; i < dblArr.length; i++) {
+      tmp[i] = PrInt.from(dblArr[i]);
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a float array to a int array using {@link PrInt#from(float)}
+   * 
+   * @param fltArr
+   *          the float array to convert
+   * @return the resulting int array.
+   * @see PrInt#from(float)
+   */
+  public static int[] from(float[] fltArr) {
+    int[] tmp = new int[fltArr.length];
+    for (int i = 0; i < fltArr.length; i++) {
+      tmp[i] = PrInt.from(fltArr[i]);
+    }
+    return tmp;
+  }
+
+  /**
+   * Converts a boolean array to a int array using {@link PrInt#from(boolean)}
+   * 
+   * @param boolArr
+   *          the boolean array to convert
+   * @return the resulting int array.
+   * @see PrInt#from(boolean)
+   */
+  public static int[] from(boolean[] boolArr) {
+    int[] tmp = new int[boolArr.length];
+    for (int i = 0; i < boolArr.length; i++) {
+      tmp[i] = PrInt.from(boolArr[i]);
+    }
+    return tmp;
+  }
+}
---
+++ src/test/java/ch/deletescape/primitives/arrays/PrIntArrayTest.java
@@ -0,0 +1,44 @@
+package ch.deletescape.primitives.arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PrIntArrayTest {
+  @Test
+  public void fromLong() {
+    assertThat(PrIntArray.from(new long[] { 1L, Long.MAX_VALUE, Long.MAX_VALUE - 1 }), is(new int[] { 1, -1, -2 }));
+  }
+
+  @Test
+  public void fromInt() {
+    assertThat(PrIntArray.from(new short[] { 1, Short.MAX_VALUE }), is(new int[] { 1, 32767 }));
+  }
+
+  @Test
+  public void fromByte() {
+    assertThat(PrIntArray.from(new byte[] { 1, Byte.MAX_VALUE }), is(new int[] { 1, 127 }));
+  }
+
+  @Test
+  public void fromChar() {
+    assertThat(PrIntArray.from(new char[] { '\0', Character.MAX_VALUE, 1 }), is(new int[] { 0, 65535, 1 }));
+  }
+
+  @Test
+  public void fromDouble() {
+    assertThat(PrIntArray.from(new double[] { 1.0, 1.3, Double.MAX_VALUE }), is(new int[] { 1, 1, 2147483647 }));
+  }
+
+  @Test
+  public void fromFloat() {
+    assertThat(PrIntArray.from(new float[] { 1f, 1.3f, Float.MAX_VALUE }), is(new int[] { 1, 1, 2147483647 }));
+  }
+
+  @Test
+  public void fromBoolean() {
+    assertThat(PrIntArray.from(new boolean[] { true, false }), is(new int[] { 1, 0 }));
+  }
+
+}
