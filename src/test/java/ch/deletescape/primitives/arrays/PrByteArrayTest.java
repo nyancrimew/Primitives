317bc44b93f7e7901fe299f1158bd170cd7aee89
deletescape <deletescape@outlook.com>
2016-12-13 08:39:26 +0100

    implement #findSequence, #insert and #append on PrByte

--- src/test/java/ch/deletescape/primitives/arrays/PrByteArrayTest.java
+++ src/test/java/ch/deletescape/primitives/arrays/PrByteArrayTest.java
@@ -63,6 +63,33 @@ public class PrByteArrayTest {
     assertThat(PrByteArray.distinct(new byte[0]), is(new byte[0]));
   }
 
+  @Test
+  public void findSequence() {
+    assertThat(PrByteArray.findSequence(new byte[] { 1, 2, 1, 3 }, (byte) 2, (byte) 1), is(1));
+    assertThat(PrByteArray.findSequence(new byte[] { 1, 2, 1, 3 }, (byte) 1, (byte) 3), is(2));
+    assertThat(PrByteArray.findSequence(new byte[] { 1, 2, 1, 3 }, (byte) 1, (byte) 1), is(-1));
+    assertThat(PrByteArray.findSequence(new byte[] { 1, 2, 1, 3 }, (byte) 3), is(3));
+    assertThat(PrByteArray.findSequence(new byte[] { 1, 2, 1, 3 }, (byte) 4), is(-1));
+  }
+
+  @Test
+  public void insert() {
+    assertThat(PrByteArray.insert(new byte[] { 1, 2, 3, 4 }, new byte[] { 10, 11 }, 2),
+        is(new byte[] { 1, 2, 10, 11, 3, 4 }));
+    assertThat(PrByteArray.insert(new byte[] { 1, 2, 3, 4 }, new byte[0], 0), is(new byte[] { 1, 2, 3, 4 }));
+    assertThat(PrByteArray.insert(new byte[] { 1, 2, 3, 4 }, new byte[] { 10, 11 }, 5),
+        is(new byte[] { 1, 2, 3, 4, 10, 11 }));
+    assertThat(PrByteArray.insert(new byte[] { 1, 2, 3, 4 }, new byte[] { 10, 11 }, -4),
+        is(new byte[] { 10, 11, 1, 2, 3, 4 }));
+  }
+
+  @Test
+  public void append() {
+    assertThat(PrByteArray.append(new byte[] { 1, 2, 3, 4 }, (byte) 10, (byte) 11),
+        is(new byte[] { 1, 2, 3, 4, 10, 11 }));
+    assertThat(PrByteArray.append(new byte[] { 1, 2, 3, 4 }), is(new byte[] { 1, 2, 3, 4 }));
+  }
+
   // Calls the #random(int) method for coverage reasons
   @Test
   public void random() {
