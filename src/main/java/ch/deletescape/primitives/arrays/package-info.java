459965ef22b5e124b9e18656712635d2e3fc376e
deletescape <deletescape@outlook.com>
2017-01-23 11:36:28 +0100

    Add Package documentation files

---
+++ src/main/java/ch/deletescape/primitives/arrays/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing all the array operation related classes
+ */
+package ch.deletescape.primitives.arrays;
---
+++ src/main/java/ch/deletescape/primitives/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing all the primitive type related classes and the Strings class
+ */
+package ch.deletescape.primitives;
